48058b15891006af5bf46a72c676049315ddf1da
pham-thien <pham.thien@outlook.com>
2017-04-04 14:20:57 -0400

    fix switch

--- app.js
+++ app.js
@@ -1,72 +1,62 @@
-// Min Max Functions
-
-// function max(numbers) {
-//  // your code here
-//   let currentMax = numbers[0];
-//   let i = 0; 
-//   while (i <= numbers.length) {
-//     if (numbers[i] > currentMax) {
-//       currentMax = numbers[i];
-//     }
-//     i++;
-//   }
-//   return currentMax;
-// }
+function max(numbers) {
+   let currentMax = numbers[0];
+   let i = 0; 
+   while (i <= numbers.length) {
+     if (numbers[i] > currentMax) {
+       currentMax = numbers[i];
+     }
+     i++;
+   }
+   return currentMax;
+ }
 
 
-// function min(numbers) {
-//  // your code here
-//   let currentMin = numbers[0];
-//   let i = 0;
-//   while (i <= numbers.length) {
-//     if (numbers[i] < currentMin) {
-//       currentMin = numbers[i];
-//     }
-//     i++;
-//   }
-//   return currentMin;
-// }
+function min(numbers) {
+   let currentMin = numbers[0];
+   let i = 0;
+   while (i <= numbers.length) {
+     if (numbers[i] < currentMin) {
+       currentMin = numbers[i];
+     }
+     i++;
+   }
+   return currentMin;
+ }
 
-//-----------------------------------------------
-
-// Average Numbers
-
-// function average(numbers) {
+function average(numbers) {
   
-//   var total = numbers.reduce(function(sum, n){
-//     return sum + n
-//   },0);
+   var total = numbers.reduce(function(sum, n){
+     return sum + n
+   },0);
   
-//  console.log(total);
-// }
+  return total / numbers.length;
+ }
 
-//----------------------------------------------
-
-//Fizz Buzz
-
-// function fizzBuzz(countTo) {
+function fizzBuzz(countTo) {
   
-//   var result = [];
-//   for (var i=1; i <= countTo; i++) {
-//       switch(true) {
-//         case i % 15 === 0:
-//           result.push("fizzbuzz");
-//           break;
+   var result = [];
+   for (var i=1; i <= countTo; i++) {
+   	let number = i % 15;
+       switch(number) {
+         case 3:
+         case 6:
+         case 9:
+         case 12:
+           result.push("fizz");
+           break;
           
-//         case i % 3 === 0:
-//           result.push("fizz");
-//           break;
+         case 5:
+         case 10:
+           result.push("buzz");
+           break;
           
-//         case i % 5 === 0:
-//           result.push("buzz");
-//           break;
+         case 0:
+           result.push("fizzbuzz");
+           break;
         
-//         default:
-//           result.push(i);
-//      }
-    
-//   }
-//   return result;
-// }
-
-//------------------------------------------------------
+         default:
+           result.push(i);
+      } 
+   }
+  return result;
+ }
